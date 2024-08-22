325cd0667da811fc50df90c34287f0f54ce371b7
saananokela <smnokela@gmail.com>
2024-08-22 10:17:20 +0300

    Added heart rate limit calculation ans styling improvements.

--- App.js
+++ App.js
@@ -4,19 +4,32 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [inputAge, setInputAge] = useState('')
-  const [lowerLimit, setLowerLimit] = useState(0)
-  const [upperLimit, setUpperLimit] = useState(0)
+  console.log('inputage on nyt: ' +  Number(inputAge))
+
+  const lowerLimit = (220  - Number(inputAge)) * 0.65
+  const noDecimalLowerLimit = lowerLimit.toFixed(0);
+  const upperLimit = (220  - Number(inputAge)) * 0.85
+  const noDecimalUpperLimit = upperLimit.toFixed(0);
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Age</Text>
+      <View style={styles.taskBox}>
+        <Text style={styles.text}>
+          Age
+        </Text>
         <TextInput
           keyboardType='number-pad'
           value={inputAge}
           style={styles.input}
-          onChange={setInputAge}
+          placeholder='Enter your age'
+          onChangeText={setInputAge}
         />
+        <Text style={styles.text}>
+          Hr limits:
+        </Text>
+        <Text style={styles.answerText}>
+          {noDecimalLowerLimit} - {noDecimalUpperLimit}
+        </Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -30,12 +43,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input :{
+  input : {
     height: 50,
     width: 200,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     backgroundColor: '#ffffff'
+
+  },
+  text : {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  answerText : {
+    marginTop: 15,
+  },
+  taskBox: {
+    borderWidth: 1,
+    padding: 25,
+    backgroundColor: '#cdddde'
+
   }
 });
