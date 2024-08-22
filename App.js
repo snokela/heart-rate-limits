a8cc0c7fc1f10bde2e3aad38b50a76d6bc981919
saananokela <smnokela@gmail.com>
2024-08-22 09:47:36 +0300

    Added age input feature using Text and TextInput components. Added some styles for TextInput.

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [inputAge, setInputAge] = useState('')
+  const [lowerLimit, setLowerLimit] = useState(0)
+  const [upperLimit, setUpperLimit] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Age</Text>
+        <TextInput
+          keyboardType='number-pad'
+          value={inputAge}
+          style={styles.input}
+          onChange={setInputAge}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a1dbdd',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input :{
+    height: 50,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#ffffff'
+  }
 });
